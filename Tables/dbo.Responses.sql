dea6467b52e0392f47cfc42b407300e042e7a3fc
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-07-04 12:28:11 +0100

    response alter

--- Tables/dbo.Responses.sql
+++ Tables/dbo.Responses.sql
@@ -1,7 +1,13 @@
 CREATE TABLE [dbo].[Responses]
 (
 [ResponsesId] [int] NOT NULL IDENTITY(1, 1),
-[ResponseDatetime] [datetime] NOT NULL,
-[ResponseAccuracy] [binary] (1) NOT NULL
+[ResponseName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ResponseDate] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ResponseTime] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ResponseScore] [int] NULL
 ) ON [PRIMARY]
+GO
+GRANT INSERT ON  [dbo].[Responses] TO [quizuser]
+GO
+GRANT SELECT ON  [dbo].[Responses] TO [quizuser]
 GO
